ff5629c5470f3fbffca99b2569ec001600c64930
kood-dev <kood.d2v@gmail.com>
2020-09-01 11:48:00 +0900

    init project

--- src/main/java/com/hello/cicdtest/IndexController.java
+++ src/main/java/com/hello/cicdtest/IndexController.java
@@ -1,4 +1,13 @@
 package com.hello.cicdtest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "hello-world";
+    }
 }
